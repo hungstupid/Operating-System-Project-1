cfa41c521cf3fad83a272627ac549f82446e1c25
Phuc <tranthienphuc3306@gmail.com>
2026-04-26 12:09:17 +0700

    4.1.1 - Speed up syscalls

--- kernel/proc.c
+++ kernel/proc.c
@@ -132,6 +132,15 @@ found:
     return 0;
   }
 
+  // Cấp phát 1 page vật lý cho usyscall
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  // Ghi PID hiện tại vào struct usyscall
+  p->usyscall->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -157,6 +166,12 @@ freeproc(struct proc *p)
 {
   if(p->trapframe)
     kfree((void*)p->trapframe);
+
+  if(p->usyscall) {
+    kfree((void*)p->usyscall);
+  }
+  p->usyscall = 0;
+  
   p->trapframe = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
@@ -202,6 +217,14 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // Map địa chỉ ảo USYSCALL tới địa chỉ vật lý của p->usyscall
+  if(mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->usyscall), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+  
   return pagetable;
 }
 
@@ -212,6 +235,7 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
